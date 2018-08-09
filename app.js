1a3099a8aed589ec9ac1ca2dd8e55f0ed51bfdb3
Connor G Cummings <37885032+cgcummings@users.noreply.github.com>
2018-08-08 21:10:34 -0700

    upload 30000

--- app.js
+++ app.js
@@ -1,55 +1,10 @@
-$.global = new Object();
 
-$.global.item = 1;
-$.global.total = 0;
-$.global.slider = 0;
+const PRINT_AUDIO = new Audio("https://www.soundjay.com/mechanical/sounds/polaroid-camera-take-picture-02.mp3");
 
-
-$(document).ready(function() 
-	{    
-	
-  var SlideCount = $('#slides li').length;	
-	var SliderWidth = SlideCount * 100;
-	var SlideWidth = 100 / SlideCount;
-	
-    $.global.total = SlideCount; 
-    
-	$('#image-carousel .handle').css('width',''+SliderWidth+'%');
-	$('#image-carousel .handle .slide').css('width',''+SlideWidth+'%');
-	
-	DragIt();
-  
-  
-  $('#right-arrow').click(function() {                   
-    var $currentstep = $.global.slider.getStep();
-    var $nextstep = parseInt($currentstep) + 1; $.global.slider.setStep($nextstep);
-     });
-  
-   $('#left-arrow').click(function() {                   
-    var $currentstep = $.global.slider.getStep();
-    var $nextstep = parseInt($currentstep) - 1; $.global.slider.setStep($nextstep);
-     });
-  
-  });
-   
-
-
-function DragIt()
-	{
-  $.global.slider = new Dragdealer('image-carousel', {
-  steps: $.global.total,
-  speed: 0.2,
-  loose: true,
-  css3: true,
-    top:0,
-    bottom:0,
-    left:0,
-    right:0,
-  requestAnimationFrame: true,
-  callback: function(x, y) {
-    $('#console').html(x);}
-  });
-	}
-	
- 
-
+function myFunction() {
+  document.getElementById("s1").style.animation= "print 4.5s";
+  document.getElementById("b").style.animation= " show 7s 4.5s";
+  document.getElementById("b").style.opacity="0";
+  document.getElementById("s1").style.top="60px";
+  PRINT_AUDIO.play();
+}
---
+++ index.php
@@ -0,0 +1,321 @@
+<html>
+
+<head>
+  <meta charset="utf-8">
+  
+  <link rel="stylesheet" href="https://necolas.github.io/normalize.css/7.0.0/normalize.css">
+    <link type="text/css" rel="stylesheet" href="style.css">
+ <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+  <meta charset="utf-8">
+  <script src="app.js"></script>
+<meta name="viewport" content ="width=device-width,initial-scale=1,user-scalable=yes" />
+  <!-- <link rel="stylesheet" href="style.css"> -->
+</head>
+
+<body>
+
+  <!-- Start Header -->
+
+  <div class="header">
+    <div class="slider">
+      <div class="container">
+        <div class="intro">
+          <h2>Cummings & Landgrave Web Development</h2>
+             <a href="#contact3">
+          <button class="my-button" type="button" name="button"> Learn More</button>
+            </a>
+        </div>
+      </div>
+    </div>
+    <div class="navbar">
+      <div class="container">
+        <h2>C<span>L</span></h2>
+        <ul>
+          <li class="active"><a href="#">Home</a></li>
+          <li><a href="#contact4">About</a></li>
+          <li><a href="#contact5">Skills</a></li>
+          <li><a href="#contact2">Contact Us</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+  <!-- End Header -->
+
+  <!-- Start Features -->
+<div id="contact3">
+  <div class="features" >
+    <div class="container">
+      <div class="feat">
+        <h2>Values</h2>
+          <p>We aim for a quality product tailored<br>to individual needs. Our company believes<br>that effective website development should<br>provide a crystal clear representation<br> of YOUR company at a reasonable cost. </p>
+      </div>
+      <div class="feat">
+        <h2>Goals</h2>
+          <p>Our goal is to provide affordable web<br>development & design with open communication<br>between client and developer. The more<br>input we receive, the better we can serve you! </p>
+      </div>
+      <div class="feat">
+        <h2>Resources</h2>
+        <p>In additoion to web developers we also<br>have a writer on staff to help you form<br> the writen content you want featured on your websight  </p>
+      </div>
+    </div>
+  </div>
+    </div>
+
+  <!-- End Features -->
+
+
+  <!-- Start About Me -->
+
+  <div class="about-me" id=contact4>
+    <div class="containerB">
+      <div class="image">
+        <img src="photo.jpg" alt="Test"  >
+      </div>
+      <div class="info">
+        <h2>About Us</h2>
+        <p>Ben Landgrave is an object oriented developer from Louisville, KY. Before entering the world of web development he worked in programming. He also writes and records his own music — he performs live downtown every Sunday!</p>
+        <p> Connor Cummings is a physics student at the University of Louisville. In addition to web development Connor is involved in research for the mu2e experiment at Fermilab, where he performs simulation and analysis. Personally, Connor is about to become a father and has just settled into a home with his wife, Rachel.</p>
+        <div class="hobbies">
+          <!-- Start Box Content -->
+          <div class="content">
+            <!-- Start Icon -->
+            <div class="icon">
+             <i class="fa fa-paint-brush fa-2x"></i>
+            </div>
+            <!-- End Icon -->
+            <!-- Start Text -->
+            <div class="text">
+              <h3>Web Design</h3>
+              <p>We became interested in design after taking a Javascript class; we soon learned that beyond this there were numerous resources available to build upon our skills. We explore all the latest trends in design to make sure you have the most up-to-date site on the market.</p>
+            </div>
+            <!-- End Text -->
+          </div>
+          <!-- End Box Content -->
+
+          <!-- Start Box Content -->
+          <div class="content">
+            <!-- Start Icon -->
+            <div class="icon">
+              <i class="fa fa-users  fa-2x"></i>
+            </div>
+            <!-- End Icon -->
+            <!-- Start Text -->
+            <div class="text">
+              <h3>Marketing</h3>
+              <p>Our services are geared toward normal people doing extraordinary things — running a small business, a non-profit organization, a sales company. We know that you do it all. We’re here so you don’t have to. </p>
+            </div>
+            <!-- End Text -->
+          </div>
+          <!-- End Box Content -->
+
+          <!-- Start Box Content -->
+          <div class="content">
+            <!-- Start Icon -->
+            <div class="icon">
+           
+            </div>
+            <!-- End Icon -->
+            <!-- Start Text -->
+                          
+            <!-- End Text -->
+          </div>
+          <!-- End Box Content -->
+ 
+          <!-- Start Box Content -->
+          <div class="content">
+            <!-- Start Icon -->
+            <div class="icon">
+              <i class="fa fa-camera fa-2x"></i>
+            </div>
+            <!-- End Icon -->
+            <!-- Start Text -->
+            <div class="text">
+              <h3>Photography</h3>
+              <p>All graphic design and photographic content courtesy of the lovely Brendan O’hern. Discover his work at the links to the right and our insight gallery</p>
+                 <div class="text"><a href="#contact7">
+                  <button class="my-button" type="button" name="button"> Gallery</button>
+                    </a>
+   
+                </div>
+                <br>
+                <br>
+                <br>
+                <br>
+            
+
+            </div>
+            <!-- End Text -->
+          </div>
+          <!-- End Box Content -->
+        </div>
+        <div class="content">
+            <div class="text">
+          <div class="cont1">
+  <div class="top"></div>
+  <div class="printerin">
+      <div id="s1">
+        <div id="b"></div>
+    </div>
+    
+  </div>
+    <div class="printer"></div>
+  
+  <div class="upper">
+    <div class="rainbow1"></div>
+  </div>
+  <div class="downer">
+    <div class="rainbow"></div>
+  </div>
+  <div class="lense"></div>
+  <button class="button" onclick="myFunction()"></button>
+  <div class="flash"></div>
+  <div class="nod"></div>
+  <div class="lable">
+    <h3>OneStep</h3>
+  </div>
+</div>
+</div>
+</div>
+      </div>
+    </div>
+  </div>
+  <!-- End About Me -->
+
+  <!-- Start My Skills -->
+
+  <div class="my-skills" id=contact5>
+    <div class="container">
+      <div class="skills">
+        <h2>Our skills</h2>
+        <p>Our staff has obtained mastery in HTML, CSS, PHP, and Javascript. We are also capable of performing data analysis with a strong background in advanced mathematics, C++, C#, and Python. </p>
+        <p>We have an additional team that specializes in graphic design, photography, and written content to add polish and chic to your website design. </p>
+          <a href="#contact2">
+        <button type="button" class="my-button" name="button" >Contact Us</button>
+          
+      </div>
+          </a>
+        
+      <div class="progress">
+        <div class="technique">
+          <h3>Html / CSS</h3>
+          <div><span style="width:83%">83% &nbsp;</span></div>
+        </div>
+        <div class="technique">
+          <h3>Php </h3>
+          <div><span style="width:88%">88% &nbsp;</span></div>
+        </div>
+        <div class="technique">
+          <h3>Javascript</h3>
+          <div><span style="width:75%">75% &nbsp;</span></div>
+        </div>
+        <div class="technique">
+          <h3>Graphic Design and Photography</h3>
+          <div><span style="width:90%">90% &nbsp;</span></div>
+        </div>
+              
+        </div>
+      </div>
+    </div>
+     
+    
+     
+  
+   
+
+  <!-- End My Skills -->
+
+
+  
+  <div class="testimonials">
+    <div class="container3" id="contact7">
+<section>
+<marquee 
+  width="100%"
+  behavior="alternate">
+<img src="1.jpg" alt="">
+  <img src="2.jpg" alt="">
+  <img src="3.jpg" alt="">
+  <img src="4.jpg" alt="">
+  <img src="5.jpg" alt="">
+  <img src="6.jpg" alt="">
+    <img src="7.jpg" alt="">
+      <img src="8.jpg" alt="">
+        <img src="9.jpg" alt="">
+          <img src="10.jpg" alt="">
+            <img src="11.jpg" alt="">
+</marquee>
+</section>
+      </div>
+      </div>
+      <!-- End Row-->
+   
+    
+     
+  </div>
+  
+      </div>
+    </div>
+  </div>
+  <!-- Start Portfolio -->
+
+  <!-- Start Contact Me -->
+  <div class="contact-me" id="contact2">
+    <div class="container">
+               
+      <div class="info">
+        <h2>Contact Us</h2>
+        <p class="paragrph">Send our developers an email. We will get back to you in 24 hours gaurenteed. We are so excited to work with you! </p>
+          
+        <address class="paragrph">
+      
+        </address>
+        <p class="paragrph">
+          <strong>Email:</strong> clwebdevelopment@outlook.com <br>
+          <strong>Phone:</strong> 502-453-1443        </p>
+    <ul>
+                    <li><a href="https://facebook.com"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                    <li><a href="https://linkedin.com"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+                    <li><a href="https://twitter.com"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+    </ul>
+          
+      </div>
+      <form class="form" action="contactform.php" method="post">
+        <label>Name *</label>
+        <input type="text" name="name" placeholder="Your Name">
+        <label>Email Adress *</label>
+        <input type="email" name="mail" placeholder="Your Email Adress">
+        <label>Message *</label>
+        <textarea name="message" rows="10" cols="80" placeholder="Your message"></textarea>
+        <div>
+             <button type="submit" name="submit" id="submit" class="form-btn semibold">Send Mail</button> 
+          </div>
+      </form>
+    </div>
+     
+  </div>
+      
+  <!-- End Contact Me -->
+
+  <!-- Start Fototer -->
+  <div class="footer">
+    <div class="container">
+      <div class="footer-folat">
+      <span class="he">Cumming & Landgrave </span>
+      <ul>
+        <li><a href="https://www.linkedin.com"><i class="fa fa-fw fa-linkedin"></i></a></li>
+        <li><a href="https://github.com/clwebdevelopment"><i class="fa fa-fw fa-github"></i></a></li>
+        <li><a href="https://twitter.com"><i class="fa fa-fw fa-twitter"></i></a></li>
+        <li><a href="mailto:clwebdevelopment@outlook.com" title="My E-mail"><i class="fa fa-fw fa-envelope"></i></a></li>
+      </ul>
+      </div>
+
+    </div>
+  </div>
+
+
+  <!-- End Fototer -->
+</body>
+</html>
+
+
